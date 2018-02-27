3e2b67ab1b9dda8665f0505b83c14082818ca877
Royce Dy <rady182@me.com>
2018-02-27 17:43:29 +0800

    added createReducer helper

---
+++ app/lib/createReducer.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function createReducer(initialState, handlers) {
+  return function reducer(state = initialState, action) {
+    if (handlers.hasOwnProperty(action.type)) {
+      return handlers[action.type](state, action);
+    }
+    return state;
+  };
+}
