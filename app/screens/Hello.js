11fa66e1ab7ecf29d7c15537882eb9809b55afdb
Royce Dy <rady182@me.com>
2018-02-28 16:34:36 +0800

    changed class name for Hello Screen

--- app/screens/Hello.js
+++ app/screens/Hello.js
@@ -15,7 +15,7 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class HelloScreen extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
