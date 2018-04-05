85ba5d71f978d990f6a0b5a0ab47034da1b2644f
Royce Dy <rady182@me.com>
2018-04-05 15:19:41 +0800

    Initial implemetation for login screen

---
+++ app/screens/Login.js
@@ -0,0 +1,187 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button, SocialIcon } from 'react-native-elements';
+import { graphql, compose } from 'react-apollo';
+import { LoginManager } from 'react-native-fbsdk';
+import {
+  LoginWithEmailMutation,
+  LoginWithFacebookMutation,
+} from '../graphql/mutations';
+
+type Props = {};
+class LoginScreen extends Component<Props> {
+  static navigationOptions = {
+    header: null,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isLoggingInWithEmail: false,
+      isLoggingInWithFacebook: false,
+    };
+  }
+
+  onChangeEmailText = text => this.setState({ email: text });
+
+  onChangePasswordText = text => this.setState({ password: text });
+
+  onPressLoginButton = () => {
+    this.setState({
+      isLoggingInWithEmail: true,
+    });
+
+    this.props
+      .loginWithEmail({
+        variables: {
+          email: this.state.email.toLowerCase(),
+          password: this.state.password,
+        },
+      })
+      .then(result => {
+        console.log('result', result);
+      })
+      .catch(e => console.log(e))
+      .finally(() => {
+        this.setState({
+          isLoggingInWithEmail: false,
+        });
+      });
+  };
+
+  onPressFacebookButton = () => {
+    this.setState({
+      isLoggingInWithFacebook: true,
+    });
+
+    LoginManager.logInWithReadPermissions(['email', 'public_profile'])
+      .then(
+        result => {
+          console.log('result:', result);
+        },
+        error => {
+          console.error('Login fail with error:', error);
+        },
+      )
+      .finally(() => {
+        this.setState({
+          isLoggingInWithFacebook: false,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.textInputContainer}>
+          <Text style={styles.textInputTitle}>Email</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={this.onChangeEmailText}
+            value={this.state.email}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+
+        <View style={styles.textInputContainer}>
+          <Text style={styles.textInputTitle}>Password</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={this.onChangePasswordText}
+            value={this.state.password}
+            secureTextEntry
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+
+        <Button
+          containerViewStyle={styles.loginButtonContainer}
+          buttonStyle={styles.loginButton}
+          rounded
+          title={this.state.isLoggingInWithEmail ? '' : 'Login'}
+          loading={this.state.isLoggingInWithEmail}
+          onPress={this.onPressLoginButton}
+          disabled={
+            this.state.isLoggingInWithEmail ||
+            this.state.isLoggingInWithFacebook
+          }
+        />
+
+        <SocialIcon
+          style={styles.facebookButton}
+          title="Sign In With Facebook"
+          button
+          type="facebook"
+          loading={this.state.isLoggingInWithFacebook}
+          onPress={this.onPressFacebookButton}
+          onLongPress={this.onPressFacebookButton}
+          disabled={
+            this.state.isLoggingInWithEmail ||
+            this.state.isLoggingInWithFacebook
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 20,
+  },
+  textInputContainer: {
+    marginTop: 20,
+  },
+  textInputTitle: {
+    fontSize: 12,
+    marginBottom: 13,
+  },
+  textInput: {
+    height: 50,
+    borderColor: '#DAE0E4',
+    backgroundColor: '#F3F6F8',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  loginButtonContainer: {
+    marginTop: 40,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  loginButton: {
+    height: 50,
+  },
+  facebookButtonContainer: {
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  facebookButton: {
+    marginLeft: 0,
+    marginRight: 0,
+    height: 50,
+  },
+});
+
+const LoginWithEmail = graphql(LoginWithEmailMutation, {
+  name: 'loginWithEmail',
+});
+
+const LoginWithFacebook = graphql(LoginWithFacebookMutation, {
+  name: 'loginWithFacebook',
+});
+
+export default compose(LoginWithEmail, LoginWithFacebook)(LoginScreen);
