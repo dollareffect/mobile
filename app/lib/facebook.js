700773d6fc18e0c18422ec32b2904def4b2a86e1
Royce Dy <rady182@me.com>
2018-04-06 12:58:58 +0800

    added fetchprofile in facebook lib

--- app/lib/facebook.js
+++ app/lib/facebook.js
@@ -1,4 +1,9 @@
-import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import {
+  LoginManager,
+  AccessToken,
+  GraphRequest,
+  GraphRequestManager,
+} from 'react-native-fbsdk';
 
 export const login = (permissions = ['email', 'public_profile']) =>
   new Promise((resolve, reject) => {
@@ -12,6 +17,29 @@ export const login = (permissions = ['email', 'public_profile']) =>
       },
       error => reject(error),
     );
+  });
+
+export const fetchProfile = (fields = 'id,name,email') =>
+  new Promise((resolve, reject) => {
+    const profileInfoCallback = (error, profileInfo) => {
+      if (error) reject(error);
+
+      resolve(profileInfo);
+    };
+
+    const profileInfoRequest = new GraphRequest(
+      '/me',
+      {
+        parameters: {
+          fields: {
+            string: fields,
+          },
+        },
+      },
+      profileInfoCallback,
+    );
+
+    new GraphRequestManager().addRequest(profileInfoRequest).start();
   });
 
 export const currentAccessToken = async () => {
