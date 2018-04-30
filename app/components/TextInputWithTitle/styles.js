ea0e5e7d3b8f30a5bc0dcff300a2ba813ca1f5be
Royce Dy <rady182@me.com>
2018-04-30 16:05:52 +0800

    added component TextInputWithTitle

---
+++ app/components/TextInputWithTitle/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {},
+  textInputTitle: {
+    fontSize: 12,
+    marginBottom: 13,
+  },
+  textInput: {
+    height: 50,
+    borderColor: '#DAE0E4',
+    backgroundColor: '#F3F6F8',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+});
